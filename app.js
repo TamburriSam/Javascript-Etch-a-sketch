f54b69bbc9f713bec57536c9afcc1df3e4c1bf81
tamburrisam <tamburrisam@gmail.com>
2021-01-30 11:46:21 -0500

    added optional grid sizes and optional colors

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const div = document.createElement('div');
 const clrBtn = document.querySelector('.clearbtn')
 
 
-gridBtn.addEventListener('click', layout16Grid)
+gridBtn.addEventListener('click', layoutGrid)
 
 clrBtn.addEventListener('click', clearGame)
 
@@ -13,20 +13,25 @@ function createColor(something, color){
 }
 
 
- function layout16Grid(){
-    playerContainer.style.gridTemplateColumns = "repeat(16, 60px)";
-    playerContainer.style.gridTemplateRows = "repeat(16, 31px)"
-    playerContainer.style.backgroundColor = 'magenta'
+ function layoutGrid(number, col, row, color){
+    number = prompt('Select a number less or equal to 100');
+    color = prompt('Select a color')
+    col = 960/number;
+    row = (Math.ceil(512/number));
+    playerContainer.style.gridTemplateColumns = `repeat(${number}, ${col}px)`;
+    playerContainer.style.gridTemplateRows = `repeat(${number}, ${row}px)`
+    playerContainer.style.backgroundColor = 'magenta';
+    gridBtn.disabled = true;
 
-    for(i=1; i<=16; i++){
-        for(j=0; j<16; j++){
+    for(i=0; i<number; i++){
+        for(j=1; j<=number; j++){
             let div1 = document.createElement('div');
             div1.classList.add('row');
             div1.style.gridRowStart = `${i}`
             playerContainer.appendChild(div1);
 
             div1.addEventListener('mouseover', function() {
-                createColor(div1, 'orange')
+                createColor(div1, `${color}`)
             })
         }
     }
